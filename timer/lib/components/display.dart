1acc758134aeb70d55dab9b1e00859140b320fbc
emilioheinz <emiliosheinz@gmail.com>
2020-02-09 14:58:33 -0300

    Changin display to receive stopwath

--- timer/lib/components/display.dart
+++ timer/lib/components/display.dart
@@ -1,6 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Display extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:timer/services/formatter.dart';
+
+class Display extends StatefulWidget {
+  final Stopwatch stopwatch;
+
+  Display({@required this.stopwatch});
+
+  @override
+  _DisplayState createState() => _DisplayState(stopwatch: stopwatch);
+}
+
+class _DisplayState extends State<Display> {
+  final Stopwatch stopwatch;
+
+  Timer timer;
+
+  _DisplayState({this.stopwatch}) {
+    timer = new Timer.periodic(new Duration(milliseconds: 30), callback);
+  }
+
+  void callback(Timer timer) {
+    if (stopwatch.isRunning) {
+      setState(() {});
+    }
+  }
+
+  getTimerContent() {
+    return formatToTimerString(stopwatch);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -8,7 +38,7 @@ class Display extends StatelessWidget {
       child: Align(
         alignment: Alignment.center,
         child: Text(
-          '00:00,00',
+          getTimerContent(),
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.w200,
